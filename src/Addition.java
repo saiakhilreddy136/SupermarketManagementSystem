b9982c3c7222782f1851ea622267bb5f372854c4
Godugunoori Sai Akhil Reddy <167629936+saiakhilreddy136@users.noreply.github.com>
2024-09-02 12:01:46 +0530

    Update Addition.java

--- src/Addition.java
+++ src/Addition.java
@@ -1,8 +1,1 @@
-class Addition{
-    public static void main(String[] args){
-        int a=20;
-        int b=30;
-        int c=a*b;
-        System.out.println("mul of"+a+" and "+b+" is:"+c);
-    }
-}
+
